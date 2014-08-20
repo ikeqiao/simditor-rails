73c63e30708db82e57ade2adc849503eda365ae2
greatghoul <greatghoul@gmail.com>
2014-08-20 11:41:06 +0800

    add precompile for vendor/images

--- lib/simditor-rails.rb
+++ lib/simditor-rails.rb
@@ -3,6 +3,9 @@ require "simditor-rails/version"
 module Simditor
   module Rails
     class Engine < ::Rails::Engine
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile << %w( loading-upload.gif )
+      end
     end
   end
 end
