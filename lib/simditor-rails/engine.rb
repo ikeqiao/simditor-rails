c9efeacc983925671f7070be413645e1539b2136
greatghoul <greatghoul@gmail.com>
2014-08-20 14:31:55 +0800

    fix assets initializers
    use `+=` not `<<` to append precompile

--- lib/simditor-rails/engine.rb
+++ lib/simditor-rails/engine.rb
@@ -1,8 +1,8 @@
 module Simditor
   module Rails
     class Engine < ::Rails::Engine
-      initializer :simditor_assets do |app|
-        app.config.assets.precompile << %w( loading-upload.gif )
+      initializer :assets do |app|
+        app.config.assets.precompile += %w( loading-upload.gif )
       end
     end
   end
